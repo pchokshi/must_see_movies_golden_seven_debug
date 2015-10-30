bf2a2874ea9a285860f95a6ab1dcb531ee00e836
pchokshi <poojachokshi@Poojas-MacBook-Air.local>
2015-10-30 11:35:45 -0500

    directors done

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,19 @@ Rails.application.routes.draw do
   # Routes for the Director resource:
   # CREATE
   get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
+  get('/create_director', { :controller => 'directors', :action => 'create_row' })
 
   # READ
   get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show_details' })
+
+  # DELETE
+  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
 
   # UPDATE
   get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
   get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
 
-  # DELETE
-  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
   #------------------------------
 
   # Routes for the Movie resource:
